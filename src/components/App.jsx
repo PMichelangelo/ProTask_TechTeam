7e5e7e3fe9643646ae905a477bb1168adea4f594
PMichelangelo <Pmichelangelo1377@gmail.com>
2024-05-01 20:18:04 +0300

    comment App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,33 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-
-import { current } from '../redux/auth/auth-operations';
-
-import AppRoutes from './AppRoutes';
-
 export const App = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(current());
-  }, [dispatch]);
-  return <AppRoutes />;
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
+  );
 };
+
+// import { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+
+// import { current } from '../redux/auth/auth-operations';
+
+// import AppRoutes from './AppRoutes';
+
+// export const App = () => {
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     dispatch(current());
+//   }, [dispatch]);
+//   return <AppRoutes />;
+// };
+
