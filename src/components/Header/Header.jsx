895fef0a3a307d237413a8d5c459bc1d7de2c5bc
Valeriy0993 <137831411+Valeriy0993@users.noreply.github.com>
2024-05-04 12:15:36 +0300

    Header
    Sidebar icon, styled-components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,12 @@
+import React from 'react';
+import SidebarIcon from './HeaderIcons/SidebarIcon';
+
 const Header = () => {
+  return (
+    <header>
+      <SidebarIcon />
+    </header>
+  );
+};
 
-}
-
-export default Header
+export default Header;
