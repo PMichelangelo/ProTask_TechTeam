1d135374b90f7078a6e21281c3d06b77d955d17b
St0Lee <staslee00@gmail.com>
2024-05-06 22:11:46 +0300

    Commit-04

--- src/components/PublicRoute/PublicRoute.jsx
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -1,21 +1,16 @@
 import { useSelector } from "react-redux";
 import { Outlet, Navigate } from "react-router-dom";
 
-import { selectIsLogin, selectToken } from "../../redux/auth/auth-selectors";
-
+import { selectIsLogin } from "../../redux/auth/auth-selectors";
 
 const PublicRoute = () => {
     const isLogin = useSelector(selectIsLogin);
-    const token = useSelector(selectToken)
 
-    if(!isLogin && token) {
-        return "Loading..."
+    if (isLogin) {
+        return <Navigate to="/home" />;
     }
 
-    if(isLogin) {
-        return <Navigate to="/" />
-    }
-    return <Outlet />
+    return <Outlet />;
 }
 
 export default PublicRoute;
