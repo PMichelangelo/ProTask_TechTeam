a16a2936fb377d8ac7af4d18c8a142b73b7067d0
Valeriy0993 <137831411+Valeriy0993@users.noreply.github.com>
2024-05-07 20:04:49 +0300

    Header
    comit1

--- src/components/Header/ThemeContext.js
+++ src/components/Header/ThemeContext.js
@@ -1,10 +1,9 @@
-// ThemeContext.js
 import React, { createContext, useState } from 'react';
 
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light'); // Початкова тема
+  const [theme, setTheme] = useState('light');
 
   const toggleTheme = () => {
     setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
@@ -16,38 +15,3 @@ export const ThemeProvider = ({ children }) => {
     </ThemeContext.Provider>
   );
 };
-
-// import React, { createContext, useState } from 'react';
-
-// export const ThemeContext = createContext();
-
-// export const ThemeProvider = ({ children }) => {
-//   const [theme, setTheme] = useState('light');
-
-//   const toggleTheme = () => {
-//     setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
-//   };
-
-//   const themeColors = {
-//     light: {
-//       background: '#B8BCFD',
-//       text: '#161616',
-//     },
-//     dark: {
-//       background: '#1F1F1F',
-//       text: '#FFFFFF',
-//     },
-//     violet: {
-//       background: '#F6F6F7',
-//       text: '#161616',
-//     },
-//   };
-
-//   return (
-//     <ThemeContext.Provider
-//       value={{ theme, toggleTheme, colors: themeColors[theme] }}
-//     >
-//       {children}
-//     </ThemeContext.Provider>
-//   );
-// };
