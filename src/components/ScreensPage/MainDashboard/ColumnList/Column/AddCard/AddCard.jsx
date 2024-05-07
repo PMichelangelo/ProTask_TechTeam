2554755f146385e0e265deede6777e97ac8fc237
Victor Petrenko <138097657+Victor-Lars-Petrenko@users.noreply.github.com>
2024-05-07 21:40:04 +0300

    updated styles

--- src/components/ScreensPage/MainDashboard/ColumnList/Column/AddCard/AddCard.jsx
+++ src/components/ScreensPage/MainDashboard/ColumnList/Column/AddCard/AddCard.jsx
@@ -3,8 +3,12 @@ import sprite from '../../../../../../images/icons.svg';
 import css from './addCard.module.css';
 
 const AddCard = () => {
+  const handleClick = event => {
+    event.target.blur();
+  };
+
   return (
-    <button className={css.addCardBtn}>
+    <button className={css.addCardBtn} onClick={handleClick}>
       <span className={css.plus}>
         <svg className={css.plusIcon}>
           <use href={`${sprite}#plus-icon`} />
