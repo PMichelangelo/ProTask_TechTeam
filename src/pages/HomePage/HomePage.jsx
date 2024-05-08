be0a81300122cf8c11e57dfe431d73c9a6bf6e25
MakEva <sher.evikm@gmail.com>
2024-05-08 14:55:03 +0300

    create new board markup

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
 
 import Header from 'components/Header/Header';
 import Sidebar from 'components/Sidebar/Sidebar';
 import ScreensPage from 'components/ScreensPage';
 
 const HomePage = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
- <div style={{ display: 'flex', height: '100%' }}>
-      <Sidebar />
+    <div style={{ display: 'flex', height: '100%' }}>
+      <Sidebar setIsMenuOpen={setIsMenuOpen} isMenuOpen={isMenuOpen} />
       <div style={{ flex: '1', display: 'flex', flexDirection: 'column' }}>
         <Header />
         <Routes>
