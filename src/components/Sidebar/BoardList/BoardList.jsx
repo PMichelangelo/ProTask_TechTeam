6ebe9589a5988f8454b7a5ec4d0a78cc3a57aea7
Victor Petrenko <138097657+Victor-Lars-Petrenko@users.noreply.github.com>
2024-05-10 20:57:15 +0300

    board actions

--- src/components/Sidebar/BoardList/BoardList.jsx
+++ src/components/Sidebar/BoardList/BoardList.jsx
@@ -1,28 +1,17 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { selectDashboards } from '../../../redux/dashboards/dashboards-selectors';
-import { fetchOneDashboard } from '../../../redux/dashboards/dashboards-operations';
 
 import css from './boardList.module.css';
+import BoardListItem from './BoardListItem/BoardListItem';
 
 const BoardList = () => {
-  const dispatch = useDispatch();
   const dashboards = useSelector(selectDashboards);
-
-  const onClick = () => {
-    dispatch(fetchOneDashboard());
-  };
 
   return (
     <ul className={css.boardList}>
       {dashboards.map(board => {
-        return (
-          <li key={board._id}>
-            <button className={css.boardBtn} onClick={onClick}>
-              {board.title}
-            </button>
-          </li>
-        );
+        return <BoardListItem key={board._id} board={board}></BoardListItem>;
       })}
     </ul>
   );
