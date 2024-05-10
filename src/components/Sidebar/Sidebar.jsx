039d9230524b67c4c502071d5f38b6cea62300d9
MakEva <sher.evikm@gmail.com>
2024-05-10 16:14:22 +0300

    commit

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,9 +5,19 @@ import Logout from './Logout/Logout';
 import NeedHelp from './NeedHelp/NeedHelp';
 import css from './sidebar.module.css';
 import { useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
+import { selectTheme } from '../../redux/theme/theme-selectors';
 
 const Sidebar = ({ setIsMenuOpen, isMenuOpen }) => {
   const menuRef = useRef(null);
+
+  const currentTheme = useSelector(selectTheme);
+  const themeClassMap = {
+    theme_dark: css.theme_dark,
+    theme_light: css.theme_light,
+    theme_violet: css.theme_violet,
+  };
+  const sidebarTheme = themeClassMap[currentTheme] || '';
 
   const handleClickOutside = event => {
     if (
@@ -28,17 +38,19 @@ const Sidebar = ({ setIsMenuOpen, isMenuOpen }) => {
 
   return (
     <div className={isMenuOpen ? css.openSidebar : css.sidebar} ref={menuRef}>
-      <div>
-        <LogoComponent />
-        <div className={css.myBoards}>
-          <h3 className={css.myBoardsTitle}>My boards</h3>
-          <CreateNewBoard />
-          <BoardList />
+      <div className={`${sidebarTheme}`}>
+        <div>
+          <LogoComponent />
+          <div className={css.myBoards}>
+            <h3 className={css.myBoardsTitle}>My boards</h3>
+            <CreateNewBoard />
+            <BoardList />
+          </div>
         </div>
-      </div>
-      <div className={css.needHelpLogout}>
-        <NeedHelp />
-        <Logout />
+        <div className={css.needHelpLogout}>
+          <NeedHelp />
+          <Logout />
+        </div>
       </div>
     </div>
   );
