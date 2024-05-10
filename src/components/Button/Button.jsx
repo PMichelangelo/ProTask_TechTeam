70f0e2949d10e722af79762253c0dd7a8ad581b5
PMichelangelo <Pmichelangelo1377@gmail.com>
2024-05-11 00:50:43 +0300

    commit

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,6 @@
 // import styles from './Button.module.css'
 const Button = ({ children, onClick }) => {
-  return <button onClick={onClick}>{children}</button>;
+  return <button onClick={onClick}>{children}!</button>;
 };
 
 export default Button;
