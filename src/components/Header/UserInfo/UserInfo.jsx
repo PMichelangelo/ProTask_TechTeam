7e83a73e9fe01ca1f7973f66b7f894a20d4d4967
Vladimir Tsymbaliuk <89538231+RichardDitrix@users.noreply.github.com>
2024-05-11 21:06:53 +0300

    Try

--- src/components/Header/UserInfo/UserInfo.jsx
+++ src/components/Header/UserInfo/UserInfo.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
+import Modal from '../../Modal/Modal';
+import UserAvatar from '../UserAvatar';
+import UserForm from '../UserForm';
 
-import Modal from './Modal';
 import { useSelector } from 'react-redux';
 import { selectTheme } from '../../../redux/theme/theme-selectors';
 
 import styles from './userInfo.module.css';
-import defaultAvatar from '../UserInfo/avatar.png';
 
 const UserInfo = ({ user }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -31,31 +32,29 @@ const UserInfo = ({ user }) => {
   const handleEditProfile = formData => {
     console.log(formData);
     closeModal();
-  }
+  };
 
   return (
     <div className={`${styles.userContainer} ${userClassName}`}>
       {user && (
-        <div className={styles.userInfo}>
-          <p className={styles.userName}>{user.name}</p>
-          <img
-            className={styles.avatar}
-            src={user.avatar || defaultAvatar}
-            alt={user.name}
-            onClick={openModal}
-            style={{ cursor: 'pointer' }}
-          />
-        </div>
+        <button onClick={openModal} className={styles.userInfo}>
+          {user?.name && <span>{user?.name}</span>}
+
+          <span
+            style={{
+              width: '32px',
+              height: '32px',
+            }}
+          >
+            <UserAvatar user={user} />
+          </span>
+        </button>
       )}
 
       <Modal isOpen={isModalOpen} onClose={closeModal}>
         <h2>Edit Profile</h2>
-        <form onSubmit={handleEditProfile}>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" defaultValue={user.name} required />
 
-          <button type="submit">Save Changes</button>
-        </form>
+        <UserForm user={user} onSubmit={handleEditProfile} />
       </Modal>
     </div>
   );
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,30 +1,20 @@
+import React, { useEffect, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 
 import css from './modal.module.css';
 import sprite from '../../images/icons.svg';
 
-import React, { useEffect } from 'react';
-import { useSelector } from "react-redux";
-import { selectTheme } from "../../redux/theme/theme-selectors";
-
 const modalRoot = document.getElementById('modal-root');
 const Modal = ({ isOpen, onClose, title, children }) => {
+  const closeModal = useCallback(
+    ({ target, currentTarget, code }) => {
+      if (target === currentTarget || code === 'Escape') {
+        onClose(false);
+      }
+    },
+    [onClose]
+  );
 
-  const currentTheme = useSelector(selectTheme)
-
-  const themeClassMap = {
-    'dark': css.theme_dark,
-    'light': css.theme_light,
-    'violet': css.theme_violet,
-  };
-
-  const screensPageTheme = themeClassMap[currentTheme] || '';
-
-  const closeModal = ({ target, currentTarget, code }) => {
-    if (target === currentTarget || code === 'Escape') {
-      onClose(false);
-    }
-  };
   useEffect(() => {
     document.addEventListener('keydown', closeModal);
 
@@ -34,13 +24,17 @@ const Modal = ({ isOpen, onClose, title, children }) => {
   return createPortal(
     <>
       {isOpen && (
-        <div onClick={closeModal} className={css.modal_wraper}>
-          <div className={`${css.modal_content} ${screensPageTheme}`}>
-            <h2 className={css.titel}>{title}</h2>
+        <div onClick={closeModal} className={css.modal_wrapper}>
+          <div className={css.modal_content}>
+            {title && <h2 className={css.titel}>{title}</h2>}
+
             <button className={css.btn_close} onClick={() => onClose(false)}>
-            <svg  className={css.item_svg}>
-             <use className={css.item_use}  href={`${sprite}#x-close-icon`}></use>
-        </svg>
+              <svg className={css.item_svg}>
+                <use
+                  className={css.item_use}
+                  href={`${sprite}#x-close-icon`}
+                ></use>
+              </svg>
             </button>
             {children}
           </div>
@@ -50,5 +44,5 @@ const Modal = ({ isOpen, onClose, title, children }) => {
     modalRoot
   );
 };
-export default Modal;
 
+export default Modal;
