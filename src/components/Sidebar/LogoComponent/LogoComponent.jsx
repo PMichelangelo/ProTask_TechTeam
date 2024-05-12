6c9efbc0a752614e79cf75ceda31bdbb09fa4e7c
Dima <dimasalabay0711@gmail.com>
2024-05-12 18:05:44 +0300

    add pseudo element for active element

--- src/components/Sidebar/LogoComponent/LogoComponent.jsx
+++ src/components/Sidebar/LogoComponent/LogoComponent.jsx
@@ -1,11 +1,20 @@
 import css from './logoComponent.module.css';
-import logo from '../../../images/logo-readme.png';
+
+import sprite from '../../../images/icons.svg';
+import CurrentTheme from 'shared/components/CurrentTheme/CurrentTheme';
 
 const LogoComponent = () => {
   return (
     <div className={css.logoComponent}>
-      <img src={logo} alt="logo" className={css.logo} />
-      {/* <h2 className={css.logoTitle}>Task Pro</h2> */}
+      {/* <img src={logo} alt="logo" className={css.logo} /> */}
+      <CurrentTheme>
+        <div className={css.logoBg}>
+          <svg className={css.logo} width="12" height="16">
+            <use href={`${sprite}#icon-logo`}></use>
+          </svg>
+        </div>
+      </CurrentTheme>
+      <h2 className={css.logoTitle}>Task Pro</h2>
     </div>
   );
 };
