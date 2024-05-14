3865bded8f90ac4d0a3543e5f4bdd3cdaddf0047
PMichelangelo <Pmichelangelo1377@gmail.com>
2024-05-15 02:13:38 +0300

    depresion 2.0

--- src/components/Header/UserAvatar/UserAvatar.jsx
+++ src/components/Header/UserAvatar/UserAvatar.jsx
@@ -6,15 +6,11 @@ const UserAvatar = ({ user }) => {
   console.log(user)
   let avatarSrc = defaultAvatar;
 
-  // Проверяем, является ли avatarURL строкой
   if (typeof user.avatarURL === 'string' && user.avatarURL.startsWith('data:image')) {
-    // Если это строка, начинающаяся с 'data:image', это база64 изображение
     avatarSrc = user.avatarURL;
   } else if (user.avatarURL instanceof File) {
-    // Если это объект File, создаем URL для предварительного просмотра
     avatarSrc = URL.createObjectURL(user.avatarURL);
   } else if (user.avatarURL) {
-    // В противном случае предполагаем, что это URL-адрес удаленного изображения
     avatarSrc = user.avatarURL;
   }
   return (
