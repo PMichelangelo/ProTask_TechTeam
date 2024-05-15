4e98010b5f036280e1a6187f3056369eba615302
St0Lee <staslee00@gmail.com>
2024-05-15 19:19:27 +0300

    Commit-fix2

--- src/components/Header/UserInfo/UserInfo.jsx
+++ src/components/Header/UserInfo/UserInfo.jsx
@@ -6,13 +6,15 @@ import UserForm from '../UserForm';
 import { selectTheme, selectToken } from '../../../redux/auth/auth-selectors';
 import styles from './userInfo.module.css';
 import { updateUserProfile } from '../../../api/user-api';
+import Loader from 'components/Loader/Loader';
 
 const UserInfo = ({ user: initialUser }) => {
   const [user, setUser] = useState(initialUser);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false); // Add loading state
   const currentTheme = useSelector(selectTheme);
-  const token = useSelector(selectToken)
-  console.log(user)
+  const token = useSelector(selectToken);
+
   const themeClassMap = {
     dark: styles.theme_dark,
     light: styles.theme_light,
@@ -26,26 +28,29 @@ const UserInfo = ({ user: initialUser }) => {
   const closeModal = () => {
     setIsModalOpen(false);
   };
-const handleEditProfile = async (updatedUserData) => {
-  console.log("updatedUserData", updatedUserData)
-  try {
-    const formData = {
-      name: updatedUserData.name || "",
-      email: updatedUserData.email || "",
-      password: updatedUserData.password || "",
-      avatar: updatedUserData.avatar || "",
-    };
-    console.log('Data being sent to server:', formData);
-    const response = await updateUserProfile(token, formData);
-    console.log('User info after update:', response);
-    setUser(response.user);
-    console.log(response.user)
 
-    closeModal();
-  } catch (error) {
-    console.error(error);
-  }
-};
+  const handleEditProfile = async (updatedUserData) => {
+    setIsLoading(true); // Set loading state to true
+    try {
+      const formData = {
+        name: updatedUserData.name || "",
+        email: updatedUserData.email || "",
+        password: updatedUserData.password || "",
+        avatar: updatedUserData.avatar || "",
+      };
+      console.log('Data being sent to server:', formData);
+      const response = await updateUserProfile(token, formData);
+      console.log('User info after update:', response);
+      setUser(response.user);
+      console.log(response.user);
+      setIsLoading(false); // Set loading state to false after request completes
+
+      closeModal();
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false); // Set loading state to false in case of error
+    }
+  };
 
   const userInfoPageTheme = themeClassMap[currentTheme] || '';
 
@@ -63,7 +68,11 @@ const handleEditProfile = async (updatedUserData) => {
         </span>
       </button>
       <Modal isOpen={isModalOpen} onClose={closeModal}>
-        <UserForm user={user} onSubmit={handleEditProfile} />
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <UserForm user={user} onSubmit={handleEditProfile} />
+        )}
       </Modal>
     </div>
   );
